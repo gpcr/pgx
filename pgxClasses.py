e820d7916aefd2d859142de6c663dfa752b6232b
jwertz01 <jwertz01@gmail.com>
2014-10-24 15:42:03 -0500

    Add gene object

--- pgxClasses.py
+++ pgxClasses.py
@@ -1,9 +1,19 @@
 class pgxRefSite(object):
-	def __init__(self, associatedAlleles, cypVariantID, gene, rsVariantID, 
-	variantGenomicPos, variantTranscriptPos):
+	def __init__(self, associatedAlleles = 'undefined', cypVariantID = 'undefined', 
+	pgxGene = 'undefined', rsVariantID = 'undefined', variantGenomicPos = 'undefined', 
+	variantTranscriptPos = 'undefined'):
 		self.associatedAlleles = associatedAlleles
 		self.cypVariantID = cypVariantID
-		self.gene = gene
+		self.pgxGene = pgxGene
 		self.rsVariantID = rsVariantID
 		self.variantGenomicPos = variantGenomicPos
 		self.variantTranscriptPos = variantTranscriptPos
+	
+		
+class pgxGene(object):						
+	def __init__ (self, name= 'undefined', defaultAllele = 'undefined', 
+	alleleFunc = 'undefined', phen = 'undefined'):
+		self.name = name
+		self.defaultAllele = defaultAllele
+		self.alleleFunc = alleleFunc	#dict of allele function ID : [allele function label, set of associated *alleles]
+		self.phen = phen	#dict of phenotype : list of corresponding allele function pairs
