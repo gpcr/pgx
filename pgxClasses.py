3a3ba914b02646c15152f778a58522b83de45b51
jwertz01 <jwertz01@gmail.com>
2014-10-28 12:30:36 -0500

    Update pgxClasses.py

--- pgxClasses.py
+++ pgxClasses.py
@@ -1,19 +1,25 @@
 class pgxRefSite(object):
 	def __init__(self, associatedAlleles = 'undefined', cypVariantID = 'undefined', 
-	pgxGene = 'undefined', rsVariantID = 'undefined', variantGenomicPos = 'undefined', 
-	variantTranscriptPos = 'undefined'):
-		self.associatedAlleles = associatedAlleles
+	geneName = 'undefined', rsVariantID = 'undefined', variantGenomicPos = 'undefined', 
+	variantTranscriptPos = 'undefined', assocAllelesParsed = 'undefined',
+	zygosity = 'undefined', readDepths = 'undefined'):
+		self.associatedAlleles = associatedAlleles  #string
 		self.cypVariantID = cypVariantID
-		self.pgxGene = pgxGene
+		self.geneName = geneName	#string
 		self.rsVariantID = rsVariantID
 		self.variantGenomicPos = variantGenomicPos
 		self.variantTranscriptPos = variantTranscriptPos
-	
+		self.assocAllelesParsed = assocAllelesParsed #set
+		self.zygosity = zygosity	#string
+		self.readDepths = readDepths  #list of read depths for each record corresponding to that set of assoc. alleles
 		
 class pgxGene(object):						
-	def __init__ (self, name= 'undefined', defaultAllele = 'undefined', 
-	alleleFunc = 'undefined', phen = 'undefined'):
+	def __init__ (self, name= 'undefined', defaultAllele = 'undefined', alleleFunc = 'undefined', 
+	phen = 'undefined', genotype = 'undefined', funcStatus = 'undefined', phenotype = 'undefined'):
 		self.name = name
 		self.defaultAllele = defaultAllele
 		self.alleleFunc = alleleFunc	#dict of allele function ID : [allele function label, set of associated *alleles]
 		self.phen = phen	#dict of phenotype : list of corresponding allele function pairs
+		self.genotype = genotype  #2-element list[*allele1, *allele2]
+		self.funcStatus = funcStatus  #2-element list [*allele1 function, *allele2 function]
+		self.phenotype = phenotype	#string
